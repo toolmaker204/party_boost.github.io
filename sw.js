efb6319fa67954ff959602de251e8d6eaf6f7961
naoyakobo <naoya.kb930@gmail.com>
2026-04-05 17:38:11 +0900

    first commit

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'nomigame-v2';
+const ASSETS = ['/index.html', '/manifest.json', '/img/logo/logo.png', '/img/home/home.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html')))
+  );
+});
